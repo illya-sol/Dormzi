b139d4700c029354c6be50a77ea8cb0c53a1e2cb
illya-sol <illya.solodky@gmail.com>
2021-09-30 22:30:42 -0700

    prettier format

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-
-import { Logo } from "@components";
+import { Logo } from '@components'
+import React from 'react'
 
 export const Header: React.FC = () => {
-    return (
-        <div className="text-center bg-gray-800">
-            <Logo />
-        </div>
-    );
-};
+   return (
+      <div className="text-center bg-gray-800">
+         <Logo />
+      </div>
+   )
+}
--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,13 +1,8 @@
-import React from "react";
-import Image from "next/image";
+import Image from 'next/image'
+import React from 'react'
 
 export const Logo: React.FC = () => {
-    return (
-        <Image
-            src="/icons/nextjs-icon.svg"
-            alt="nextjs"
-            width="96"
-            height="58"
-        />
-    );
-};
+   return (
+      <Image src="/icons/nextjs-icon.svg" alt="nextjs" width="96" height="58" />
+   )
+}
